44b3f8a77bbef4505a611a3713fd1328ea7eefc1
Yunus Emre Akkaş <Y.Emre_Akkas@outlook.com>
2025-11-07 21:51:01 +0300

    feat(group-settings): add deleteChat and leaveChat methods
    - Implemented deleteChat and leaveChat in GroupSettingsComponent
    - Refactored subscriptions to object-based RxJS style
    - Improved error handling

--- ChatNestFullStack/ChatNest-app/src/app/features/chat/group-settings/group-settings.component.ts
+++ ChatNestFullStack/ChatNest-app/src/app/features/chat/group-settings/group-settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ChatService } from '../services/chat.service';
 import { RemoveUserRequestDTO, SetGroupAdminRequestDTO, UpdateGroupNameRequestDTO } from 'src/app/models/chats-request';
 import { AuthService } from 'src/app/services/auth.service';
@@ -23,7 +23,8 @@ export class GroupSettingsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private chatService: ChatService,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private router:Router) { }
 
   ngOnInit(): void {
     this.chatID = this.route.snapshot.paramMap.get('id')!;
@@ -34,17 +35,20 @@ export class GroupSettingsComponent implements OnInit {
   }
 
   loadMembers(): void {
-    this.chatService.getChatMembers(this.chatID).subscribe((res: GetChatMembersResponseModel) => {
-      if (res.messageID === 1) {
-        this.members = res.members;
-        
-        this.currentUserIsAdmin = this.members.find(m => m.userID === this.currentUserID)?.isAdmin ?? false;
-        console.log('currentUserID:', this.currentUserID);
-        console.log('members:', this.members);
+    this.chatService.getChatMembers(this.chatID).subscribe({
+      next: (res:GetChatMembersResponseModel) => {
+        if (res.messageID === 1) {
+          this.members = res.members;
+          
+          this.currentUserIsAdmin = this.members.find(m => m.userID === this.currentUserID)?.isAdmin ?? false;
+          console.log('currentUserID:', this.currentUserID);
+          console.log('members:', this.members);
 
 
-      } else {
-        console.error(res.messageDescription);
+        }
+        else {
+          console.error(res.messageDescription);
+        }
       }
     });
   }
@@ -56,11 +60,18 @@ export class GroupSettingsComponent implements OnInit {
       userID: this.currentUserID
     };
 
-    this.chatService.updateGroupName(payload).subscribe(res => {
-      if (res.messageID === 1) {
-        console.log('Group name updated');
-      } else {
-        console.error(res.messageDescription);
+    this.chatService.updateGroupName(payload).subscribe({
+      next: res =>{
+        if (res.messageID === 1) {
+          alert('Group name updated');
+        }
+        else {
+          alert(res.messageDescription);
+        }
+      },
+      error: err => {
+        alert(`Group name could not be updated: ${err.error?.messageDescription || err.message}`);
+        console.error('HTTP error:', err);
       }
     });
   }
@@ -73,56 +84,61 @@ export class GroupSettingsComponent implements OnInit {
       makeAdmin: true
     };
 
-    this.chatService.setGroupAdmin(payload).subscribe(res => {
-      if (res.messageID === 1) {
-        member.isAdmin = true;
-      } else {
-        console.error(res.messageDescription);
+    this.chatService.setGroupAdmin(payload).subscribe({
+      next: res => {
+        if (res.messageID === 1) {
+          member.isAdmin = true;
+        }
+        else {
+          alert(res.messageDescription);
+        }
+      },
+      error: err => {
+        alert(`Could not set admin: ${err.error?.messageDescription || err.message}`);
+        console.error('HTTP error:', err);
       }
     });
   }
 
-addUser(newUserId: string): void {
-  const ids = this.newUserId
-    .split(',')
-    .map(id => id.trim())
-    .filter(id => id.length > 0);
+  addUser(newUserId: string): void {
+    const ids = this.newUserId
+      .split(',')
+      .map(id => id.trim())
+      .filter(id => id.length > 0);
 
-  const payload = {
-    chatID: this.chatID,
-    userIDs: ids, // Array of user IDs
-    adminID: this.currentUserID
-  };
-  
-  this.chatService.addUserToChat(payload).subscribe({
-    next: res => {
-      if (res.messageID === 1) {
-        // Return only new members
-        res.members.forEach(m => {
-          if (!this.members.find(x => x.userID === m.userID)) {
-            this.members.push(m);
-          }
-        });
-        this.newUserId = ''; // Clean input field
-      } else {
-        console.error('Business error:', res.messageDescription);
+    const payload = {
+      chatID: this.chatID,
+      userIDs: ids, // Array of user IDs
+      adminID: this.currentUserID
+    };
+    
+    this.chatService.addUserToChat(payload).subscribe({
+      next: res => {
+        if (res.messageID === 1) {
+          // Return only new members
+          res.members.forEach(m => {
+            if (!this.members.find(x => x.userID === m.userID)) {
+              this.members.push(m);
+            }
+          });
+          this.newUserId = ''; // Clean input field
+        } 
+        else {
+          console.error('Business error:', res.messageDescription);
+        }
+      },
+      error: err => {
+      // const backendMsg = err.error?.messageDescription || err.message;
+        const backendMsg = err.error?.messageDescription || err.message;
+        alert(`Users could not be added: ${backendMsg}`);
+        console.error('HTTP error:', err);
+
+      },
+      complete: () => {
+        console.log('Add user request completed');
       }
-    },
-    error: err => {
-    // const backendMsg = err.error?.messageDescription || err.message;
-      const backendMsg = err.error?.messageDescription || err.message;
-      alert(`Users could not be added: ${backendMsg}`);
-      console.error('HTTP error:', err);
-
-    },
-    complete: () => {
-      console.log('Add user request completed');
-    }
-  });
-}
-
-
-
+    });
+  }
 
   removeUser(member: ChatMember): void {
     const payload: RemoveUserRequestDTO = {
@@ -131,14 +147,63 @@ addUser(newUserId: string): void {
       adminID: this.currentUserID
     };
 
-    this.chatService.removeUserFromChat(payload).subscribe(res => {
+    this.chatService.removeUserFromChat(payload).subscribe({
+      next: res => {
       if (res.messageID === 1) {
         this.members = this.members.filter(m => m.userID !== member.userID);
-      } else {
+      }
+      else {
         console.error(res.messageDescription);
+        }
+      },
+      error: err => {
+        alert(`User could not be removed: ${err.error?.messageDescription || err.message}`);
+        console.error('HTTP error:', err);
       }
     });
   }
 
+  deleteChat(): void {
+    this.chatService.deleteChat(this.chatID, this.currentUserID).subscribe(
+      {
+        next: res => {
+          if (res.messageID === 1) {
+            alert('Chat deleted successfully');
+            this.router.navigate(['/chats']);
+          }
+          else {
+            alert(res.messageDescription);
+          }
+        },
+        error: err => {
+          alert(`Chat could not be deleted: ${err.error?.messageDescription || err.message}`);
+        },
+        complete: () => {
+          console.log('Delete chat request completed');
+        }
+      }
+    )
+  }
 
+  leaveChat(): void {
+    this.chatService.leaveChat(this.chatID, this.currentUserID).subscribe(
+      {
+        next: res => {
+          if (res.messageID === 1) {
+            alert('You have left the chat');
+            this.router.navigate(['/chats']);
+          }
+          else {
+            alert(res.messageDescription);
+          }
+        },
+        error: err => {
+          alert(`Could not leave the chat: ${err.error?.messageDescription || err.message}`);
+        },
+        complete: () => {
+          console.log('Leave chat request completed');
+        }
+      }
+    );
+  }
 }
