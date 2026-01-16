2fba12401fab74986d90abeb8f6b266a26324207
Yunus Emre Akkaş <Y.Emre_Akkas@outlook.com>
2026-01-16 19:17:57 +0300

    Upgrade to Angular 18 with Application Builder migration

--- ChatNestFullStack/ChatNest-app/src/app/app.module.ts
+++ ChatNestFullStack/ChatNest-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -12,23 +12,17 @@ import { UserModule } from './features/user/user.module';
 import { FriendshipModule } from './features/friendship/friendship.module';
 import { ChatModule } from './features/chat/chat.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AuthenticateModule,
-    FormsModule,
-    HttpClientModule,
-    UserModule,
-    FriendshipModule,
-    ChatModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor , multi: true }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        AuthenticateModule,
+        FormsModule,
+        UserModule,
+        FriendshipModule,
+        ChatModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
